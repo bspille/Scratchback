fdae1619d88500a108643001665f047b5a5160d7
Chris <chriscallanjr@me.com>
2017-05-05 14:59:57 -0400

    finish database seed function

--- databaseSeeding.js
+++ databaseSeeding.js
@@ -3,7 +3,7 @@ var jobCategories = ["Accounting/Finance", "Automotive", "Carpentry", "General L
 "Construction", "Plumbing", "HVAC", "Consultation", "Landscaping/Gardening", "Graphic Design",
 "Photography", "Culinary Services", "Computer Programming", "Academic Tutoring"];
 
-
+var dollarSigns = ["$", "$$", "$$$", "$$$$", "$$$$$"];
 // *******************************************************************************************
 // WARNING ONLY USE ONCE TO SEED DATABASE, ERASE FUNCTION CALL AFTER USING ONCE
 // OR ELSE YOU WILL HAVE A VERY VERY BAD TIME
@@ -12,21 +12,28 @@ var jobCategories = ["Accounting/Finance", "Automotive", "Carpentry", "General L
 function databaseSeeder(){
   for (var i = 0; i <= 100; i++) {
     var randomName = faker.name.findName();
-    var randomEmail = faker.internet.email();
     var randomUsername = faker.internet.userName();
     var randomPassword = faker.internet.password();
-    var randomAvatar = faker.internet.avatar();
+    var randomEmail = faker.internet.email();
     var randomJob = jobCategories[Math.floor(Math.random() * jobCategories.length)];
     var randomSpecialization = faker.name.jobType();
     var randomLooking = jobCategories[Math.floor(Math.random() * jobCategories.length)];
+    var randomJobCost = dollarSigns[Math.floor(Math.random() * dollarSigns.length)];
+    var randomZipCode = faker.address.zipCode();
+    var randomAvatar = faker.internet.avatar();
 
-    console.log(randomName);
-    console.log(randomEmail);
-    console.log(randomUsername);
-    console.log(randomPassword);
-    console.log(randomAvatar);
-    console.log(randomJob);
-    console.log(randomSpecialization);
-    console.log(randomLooking);
+    db.Users.create({
+      fullName: randomName,
+      userName: randomUsername,
+      password: randomPassword,
+      email: randomEmail,
+      jobskill: randomJob,
+      specialization: randomSpecialization,
+      lookingFor: randomLooking,
+      jobCost: randomJobCost,
+      zip: randomZipCode,
+      avatar: randomAvatar
+    });
+
   }
 }
