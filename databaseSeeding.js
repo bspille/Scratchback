bd651c18d96243bfe39eacdabe920831272577ea
Chris <chriscallanjr@me.com>
2017-05-06 11:31:28 -0400

    fix seeder function

--- databaseSeeding.js
+++ databaseSeeding.js
@@ -1,15 +1,19 @@
-// can also use this array elsewhere to populate forms
-var jobCategories = ["Accounting/Finance", "Automotive", "Carpentry", "General Labor",
-"Construction", "Plumbing", "HVAC", "Consultation", "Landscaping/Gardening", "Graphic Design",
-"Photography", "Culinary Services", "Computer Programming", "Academic Tutoring"];
+var db = require('./models');
 
-var dollarSigns = ["$", "$$", "$$$", "$$$$", "$$$$$"];
+
 // *******************************************************************************************
 // WARNING ONLY USE ONCE TO SEED DATABASE, ERASE FUNCTION CALL AFTER USING ONCE
 // OR ELSE YOU WILL HAVE A VERY VERY BAD TIME
 // *******************************************************************************************
 
-function databaseSeeder(){
+module.exports = function(){
+  // can also use this array elsewhere to populate forms
+  var jobCategories = ["Accounting/Finance", "Automotive", "Carpentry", "General Labor",
+  "Construction", "Plumbing", "HVAC", "Consultation", "Landscaping/Gardening", "Graphic Design",
+  "Photography", "Culinary Services", "Computer Programming", "Academic Tutoring"];
+
+  var dollarSigns = ["$", "$$", "$$$", "$$$$", "$$$$$"];
+
   for (var i = 0; i <= 100; i++) {
     var randomName = faker.name.findName();
     var randomUsername = faker.internet.userName();
